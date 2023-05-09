9f28a0d94f8db4c0d276d1aa226170eababcd7a1
dembasow98 <dastech1998@gmail.com>
2023-05-09 19:35:45 +0300

    Fixed the tailwindcss unabled.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import './css/main.css';
+//import './css/main.css';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { createRoot } from 'react-dom/client';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
