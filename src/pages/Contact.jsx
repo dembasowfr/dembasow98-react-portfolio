6df165b38f3db0f74fee5d9c5769a72512361408
dembasow98 <dastech1998@gmail.com>
2023-05-17 02:07:16 +0300

    Updated contact and resume UI.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,12 @@ import ContactForm from '../components/contact/ContactForm';
 
 const Contact = () => {
 	return (
+		<div className="w-full flex flex-col justify-center content-center">
+			<div className="w-full flex items-center justify-center mt-20 text-center">
+				<p className="font-general-medium text-2xl sm:text-4xl text-ternary-dark dark:text-ternary-light">
+					Get in Touch
+				</p>
+			</div>
 		<motion.div
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
@@ -12,11 +18,13 @@ const Contact = () => {
 				duration: 0.5,
 				delay: 0.1,
 			}}
-			className="container mx-auto flex flex-col-reverse lg:flex-row py-5 lg:py-10 lg:mt-10"
+			className="container mx-auto flex flex-col-reverse lg:flex-row py-5 lg:py-8 lg:mt-8"
 		>
+			
 			<ContactForm />
 			<ContactDetails />
 		</motion.div>
+		</div>
 	);
 };
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,12 @@ import {ResumeHeader, ResumeMain, ResumeFooter} from '../components/resume'
 
 const Resume = () =>{
     return (
-        <div className = "w-full flex justify-center content-center">
+        <div className = "w-full flex flex-col items-center justify-center">
+            <div className="w-full flex items-center justify-center mt-20 mb-6 ">
+				<p className="font-general-medium text-2xl sm:text-4xl text-ternary-dark dark:text-ternary-light">
+					My Resume
+				</p>
+			</div>
             <div className="border border-gray-300 rounded-sm shadow-2xl py-10 px-10 w-4/5 mt-10 mb-10">
                 <ResumeHeader />
                 <ResumeMain />
@@ -12,6 +17,5 @@ const Resume = () =>{
       </div>
     )
 }
-
 
 export default Resume;
